4a2ee8cd8d8f43481cbc4cbfc6a55e47d953de02
Michael Quarne <mikedq1225@gmail.com>
2020-06-09 18:39:33 -0700

    changed database to AWS database - working on localhost

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql")
 const config = require("config")
 const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: config.get("db.host"),
-  user: config.get("db.user"),
-  password: config.get("db.password"),
-  database: config.get("db.database")
+	connectionLimit: 10,
+	host: config.get("awsDB.host"),
+	user: config.get("awsDB.user"),
+	password: config.get("awsDB.password"),
+	database: config.get("awsDB.database"),
 })
 
 module.exports = pool
