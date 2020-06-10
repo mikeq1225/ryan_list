b3f72640cb50018762cfdcd686b6a9b7e2937a0c
Michael Quarne <mikedq1225@gmail.com>
2020-06-10 09:16:50 -0700

    added heroku build

--- client/src/index.js
+++ client/src/index.js
@@ -2,21 +2,21 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./styles/base.css"
 import App from "./components/App"
-import * as serviceWorker from "./serviceWorker"
+// import * as serviceWorker from "./serviceWorker"
 import { Provider } from "react-redux"
 import store from "./redux/store"
 import { BrowserRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById("root")
+	<Provider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>,
+	document.getElementById("root")
 )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+// serviceWorker.unregister()
