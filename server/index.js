d654bde966a6aa2725e4eb4ee84211ac54117d5f
Michael Quarne <mikedq1225@gmail.com>
2020-06-12 16:45:16 -0700

    Server can now display page

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const path = require("path")
-const categoriesRoutes = require("./server/routes/categories")
+const categoriesRoutes = require("./routes/categories")
 const app = express()
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")))
+app.use(express.static(path.join(__dirname, "public")))
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
@@ -13,7 +13,7 @@ app.use("/api", categoriesRoutes)
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname + "/client/build/index.html"))
+	res.sendFile(path.join(__dirname + "/public/index.html"))
 })
 
 const port = process.env.PORT || 3001
---
+++ server/public/precache-manifest.cbd5c55957bf790ae9daab4867397949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2ec0884248642397f695ea4ad3d37e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d63de941bca6ad9d31f",
+    "url": "/static/css/main.5679e6a5.chunk.css"
+  },
+  {
+    "revision": "a75807bc5dba9cb1f7a9",
+    "url": "/static/js/2.2e6e3d2d.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.2e6e3d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d63de941bca6ad9d31f",
+    "url": "/static/js/main.1f1ca1e2.chunk.js"
+  },
+  {
+    "revision": "ef3bac3e4f5618869d87",
+    "url": "/static/js/runtime-main.9d40f716.js"
+  }
+]);
