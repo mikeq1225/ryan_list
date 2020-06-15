ddf26da79fdd2ccdef434c268094c8b9afce4313
Michael Quarne <mikedq1225@gmail.com>
2020-06-15 14:03:12 -0700

    added heroku config

--- server/db.js
+++ server/db.js
@@ -1,14 +1,23 @@
 const mysql = require("mysql")
 const config = require("config")
 
-// for accessing aws database
+// for pushing to Heroku to use its config
 const pool = mysql.createPool({
 	connectionLimit: 10,
-	host: config.get("awsDB.host"),
-	user: config.get("awsDB.user"),
-	password: config.get("awsDB.password"),
-	database: config.get("awsDB.database"),
+	host: process.env.host,
+	user: process.env.user,
+	password: process.env.password,
+	database: process.env.database,
 })
+
+// for accessing aws database
+// const pool = mysql.createPool({
+// 	connectionLimit: 10,
+// 	host: config.get("awsDB.host"),
+// 	user: config.get("awsDB.user"),
+// 	password: config.get("awsDB.password"),
+// 	database: config.get("awsDB.database"),
+// })
 
 // for accessing local database
 // const pool = mysql.createPool({
